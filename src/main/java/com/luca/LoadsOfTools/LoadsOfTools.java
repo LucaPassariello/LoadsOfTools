26e4e236ab88c7a3f33b061a164dd170582a9577
LucaPassariello <lucacoding@gmail.com>
2016-02-27 21:53:08 +0000

    Minecraft Basis

---
+++ src/main/java/com/luca/LoadsOfTools/LoadsOfTools.java
@@ -0,0 +1,19 @@
+package com.luca.LoadsOfTools;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "LoadsOfTools", name = "Loads Of Tools!", version = "1.8.9-1.0")
+public class LoadsOfTools
+{
+    @Mod.EventHandler
+    public void preInit()
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init()
+    {
+        
+    }
+}
